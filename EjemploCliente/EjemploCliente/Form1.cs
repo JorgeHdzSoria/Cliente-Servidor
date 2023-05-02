3c2cff54450db860bdc748ae877c6557da1f6503
JorgeHdzSoria <jorge.hdzsoria@gmail.com>
2023-05-01 22:46:52 -0500

    Server 1.0

---
+++ EjemploCliente/EjemploCliente/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace EjemploCliente
+{
+    public partial class Form1 : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                NetworkStream serverStream = clientSocket.GetStream();
+
+                byte[] outStream = System.Text.Encoding.ASCII.GetBytes(textBox1.Text + "$");
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clientSocket.Connect("127.0.0.1", 8888);
+        }
+    }
+}
+
---
+++ EjemploServidor/EjemploServidor/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Sockets;
+
+
+namespace Servidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataFromClient = "";
+            TcpListener serverSocket = new TcpListener(8888);
+            int requestCount = 0;
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+            Console.WriteLine(" >> Server iniciado");
+            clientSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine(" >> Conexion exitosa");
+
+            while ((true))
+            {
+                try
+                {
+                    NetworkStream networkStream = clientSocket.GetStream();
+                    
+                    try 
+                    {
+                        dataFromClient = "";
+                        byte[] bytesFrom = new byte[1000000];
+                        networkStream.ReadTimeout = 50;
+
+                        //Leer mensaje
+                        networkStream.Read(bytesFrom, 0, clientSocket.ReceiveBufferSize);
+
+                        //Codificar string a ascii
+                        dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                        dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                        networkStream.Flush();
+                        Console.WriteLine(dataFromClient);
+                    }
+                    catch (Exception ex)
+                    {
+                        //Esto lo use para debuguear
+                        //Console.WriteLine(" >> Esperando mensaje");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" >> Error de inicio");
+                }
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> exit");
+            Console.ReadLine();
+        }
+    }
+}
